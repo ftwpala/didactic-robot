c2459e2c228745415c5f08dd64c7228755d92e78
Ftwpala <ftwpala@abv.bg>
2021-08-25 18:01:29 +0300

    Changed from class-based to functional component, added webpack to seperate CSS from HTML. Added Bootstrap for easier expirementation with spacing, positioning etc.

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,86 +1,75 @@
-import { Component } from "react";
+import React, {useRef} from "react";
+import './Todos.css';
 
-export default class Todos extends Component {
-  state = {
-    newTodo: "",
-    todos: ["hello there", "general kenobi"],
-  };
 
-  onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!this.state.newTodo) return;
+export const Todos = () => {
+  const [newTodoInput, setNewTodoInput] = React.useState("");
+  const [todos, setTodos] = React.useState(["hello there", "general kenobi"]);
 
-    const todos = [this.state.newTodo, ...this.state.todos];
-    this.setState({ newTodo: "", todos });
-  };
+  const addInput = useRef<HTMLInputElement>(null);
 
-  removeTodo = (removeIndex: number) => {
-    const todos = this.state.todos.filter((_, index) => index !== removeIndex);
-    this.setState({ todos });
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: 500,
-          margin: "0 auto",
-          padding: 8,
-        }}
-      >
-        <h2 style={{ textAlign: "center" }}>Todo</h2>
-        <form
-          onSubmit={this.onSubmit}
-          style={{ display: "flex", marginBottom: 8 }}
-        >
-          <input
-            type="text"
-            name="newTodo"
-            id="newTodo"
-            value={this.state.newTodo}
-            onChange={(e) => this.setState({ newTodo: e.target.value })}
-            placeholder="Fix the thing.."
-            style={{
-              display: "inline-flex",
-              flex: 1,
-              padding: 4,
-              border: "1px solid #eaeaea",
-              marginRight: 4,
-            }}
-          />
-          <button
-            type="submit"
-            style={{ borderColor: "#eaeaea", backgroundColor: "#fff" }}
-          >
-            Add
-          </button>
-        </form>
-        <div>
-          {this.state.todos.length === 0 && (
-            <div style={{ textAlign: "center" }}>Add some todos</div>
-          )}
-          {this.state.todos.map((todo, i) => (
-            <div
-              key={`${todo}-${i}`}
-              style={{
-                padding: 4,
-                borderBottom: "1px solid #ccc",
-                display: "flex",
-              }}
-            >
-              <span style={{ flex: 1 }}>{todo}</span>
-              <span
-                style={{ cursor: "pointer" }}
-                onClick={() => this.removeTodo(i)}
-              >
-                &times;
-              </span>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+  const focusAddTodoInput = () =>{
+    if(addInput.current)
+    {
+      addInput.current.focus();  
+    }
   }
-}
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!newTodoInput){
+      focusAddTodoInput();
+      return;
+    }
+
+    const todostemp = [newTodoInput, ...todos];
+    setTodos(todostemp);
+    setNewTodoInput("");
+
+    focusAddTodoInput();
+  };
+
+  const removeTodo = (removeIndex: number) => {
+    const todostemp = todos.filter((_, index) => index !== removeIndex);
+    setTodos(todostemp);
+  };
+
+  return (
+    <div className="main">
+      <h2>Todo</h2>
+      <form id="newTodoForm" onSubmit={onSubmit} >
+        <input
+          type="text"
+          name="newTodoInput"
+          id="newTodoInput"
+          ref={addInput}
+          value={newTodoInput}
+          onChange={(e) => setNewTodoInput(e.target.value)}
+          placeholder="Fix the thing.."
+        />
+        <button
+          id="addTodoButton"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+      <div>
+        {todos.length === 0 && (
+          <div>Add some todos</div>
+        )}
+        {todos.map((todo, i) => (
+          <div
+            key={`${todo}-${i}`}
+            className="todoItem"
+          >
+            <span className="todoItemText">{todo}</span>
+            <span className="todoItemButton" onClick={() => removeTodo(i)}>
+              &times;
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
